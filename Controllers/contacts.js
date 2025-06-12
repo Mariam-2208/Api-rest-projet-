441ed0dd94a628bc71afc5119b74cfa40048e950
AshleyKng <ashleylonking16@gmail.com>
2025-06-12 02:21:28 +0200

    Ajout des dossiers contact

---
+++ Controllers/contacts.js
@@ -0,0 +1,54 @@
+const Contact = require('../Modeles/contact');
+
+
+
+
+exports.create = (req, res, next) => {
+   
+    const contact = new Contact({
+        nom: req.body.nom,
+        prenom: req.body.prenom,
+        telephone: req.body.telephone
+
+    }).save()
+        .then(data => {
+            
+            res.status(201).json(data);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+};
+
+exports.delete = (req, res, next) => {
+    // TODO delete files
+    Contact.findByIdAndDelete({ _id: req.params.id })
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: "Success",
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
+};
+
+exports.patch = (req, res, next) => {
+
+    Contact.findByIdAndUpdate(req.params.id, {
+        nom: req.body.nom,
+        prenom: req.body.prenom,
+        telephone: req.body.telephone,
+    }, {new: true}, function (err, data) {
+        if (err) {
+            res.send({state: "erreur update article"})
+        }
+        res.send(data);
+    })
+}
