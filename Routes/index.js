642e72c1f5d7f39bad1c90c7d27e0be27d13278a
nrbouya <nour_bouyanfif07@github.com>
2025-06-12 17:04:07 +0200

    fix

--- Routes/index.js
+++ Routes/index.js
@@ -4,8 +4,8 @@ const ordreRoutes = require('./ordre');
 
 module.exports = (app) => {
    
-  //  app.use('/api/v1/article', articleRoutes);
-    //app.use('/api/v1/city', cityRoutes);
+  //app.use('/api/v1/article', articleRoutes);
+ //app.use('/api/v1/city', cityRoutes);
     app.use('/api/v1/ordre',ordreRoutes);
 
 };
