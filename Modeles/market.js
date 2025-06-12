82f42abf8819713ad7558659fa5d63994458d96c
childrick <childrickp@gmail.com>
2025-06-12 14:49:28 +0200

    Ajout du modèle, contrôleur, et route pour Market

---
+++ Modeles/market.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const MarketSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  location: String,
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Market', MarketSchema);
