2a908fa413610c17f64c092f7a84728547fb9eb6
Rock Well Kenfack <RockWell.Kenfack1@arrow.com>
2025-06-13 08:56:53 +0200

    fix projet

--- Routes/index.js
+++ Routes/index.js
@@ -1,11 +1,11 @@
-//const  articleRoutes = require('./article');
-//const  cityRoutes = require('./city');
+const  articleRoutes = require('./article');
+const  cityRoutes = require('./city');
 const ordreRoutes = require('./ordre');
 
 module.exports = (app) => {
    
-  //app.use('/api/v1/article', articleRoutes);
- //app.use('/api/v1/city', cityRoutes);
+  app.use('/api/v1/article', articleRoutes);
+ app.use('/api/v1/city', cityRoutes);
     app.use('/api/v1/ordre',ordreRoutes);
 
 };
