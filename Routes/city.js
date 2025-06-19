3d6e4610ac9dd81dfee195343542577f58bfc20e
rockwe <32392505+rockwe@users.noreply.github.com>
2025-06-19 18:39:43 +0200

    Update city.js

--- Routes/city.js
+++ Routes/city.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const productController = require('../controllers/city');
+const productController = require('../Controllers/city');
 
 router.post('/create', productController.create);
 router.delete('/:id', productController.delete);
