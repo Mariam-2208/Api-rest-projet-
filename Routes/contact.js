9df828a1091b4ce324c8d393ac47053d85398e64
rockwe <32392505+rockwe@users.noreply.github.com>
2025-06-19 18:43:50 +0200

    Update contact.js

--- Routes/contact.js
+++ Routes/contact.js
@@ -1,11 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-<<<<<<< HEAD
 const productController = require('../Controllers/contacts');
-=======
-const productController = require('.../controllers/contact');
->>>>>>> 8367ce8 (mise à jour)
 
 router.post('/create', productController.create);
 router.delete('/:id', productController.delete);
